5d1d68f3bd1028f7f0676feae8ccb91345bfb93f
salmasamirr <126980468+salmasamirr@users.noreply.github.com>
2023-08-07 23:19:21 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// For Task 1 
+
+// 1 - Inctroduce Your Self (Name , age  , faculty , ) in varriable and write it in HTML Using JS Note using temolate Literal
+
+        //Write Code Here
+        // Note : you can write in HTML using  document.write(varribale_Name) ; 
+
+
+// 2 - You Have Number 
+
+let str = "20000.432423" ; 
+
+console.log("12312312312") //convert String To Number Using to Two different Method
+
